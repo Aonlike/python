099b658f0cb1d07d7087021c0d002bdf0ce7fc51
Martin Ngutswen <martinngutswen02@gmail.com>
2025-07-15 17:46:52 +0100

    first commit

---
+++ Exercise2.4.py
@@ -0,0 +1,23 @@
+sum = 27.5 + 2
+
+print(sum)
+
+subtraction = 27.5 - 2
+
+print(subtraction)
+
+multiplication = 27.5 * 2
+
+print(multiplication)
+
+division = 27.5 / 2
+
+print(division)
+
+flat_division = 27.5 // 2
+
+print(flat_division)
+
+square = 27.5 ** 2
+
+print(square)
